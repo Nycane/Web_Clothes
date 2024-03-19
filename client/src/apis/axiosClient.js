40623eb2f48d1fde90bd296f1610828474cbee9a
doit-now <hung0925068118@gmail.com>
2024-03-19 15:33:58 +0700

    update

--- client/src/apis/axiosClient.js
+++ client/src/apis/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const env = process.env.NODE_ENV;
 const baseUrl= env==="development" ? process.env.REACT_APP_API_LOCAL:process.env.REACT_APP_API_URL;
+console.log(baseUrl)
 const axiosClient = axios.create({
     baseURL: baseUrl,
     headers: {
