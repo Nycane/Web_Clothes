33c0256427a75b5cdd89f58e29ed2e220af1c0fa
doit-now <hung0925068118@gmail.com>
2024-03-19 18:16:38 +0700

    update

--- sever/service/authService.js
+++ sever/service/authService.js
@@ -105,7 +105,7 @@ class authService {
         path: "/",
         maxAge: 30 * 24 * 60 * 60 * 1000,
         secure: false,
-        samSite: "strict",
+        sameSite: "strict",
       });
     } else {
       console.error("Invalid data");
