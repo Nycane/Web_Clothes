3c0e0a64d08c3cae551be41d12199799edd32237
doit-now <hung0925068118@gmail.com>
2024-03-19 20:00:55 +0700

    update

--- sever/configs/passport.js
+++ sever/configs/passport.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const authSocialService = require('../service/authSocialService');
-const userService = require('../service/userService');
-const { v4: uuidv4 } = require('uuid');
+const authSocialService = require("../service/authSocialService");
+const userService = require("../service/userService");
+const { v4: uuidv4 } = require("uuid");
 var passport = require("passport");
 const constant = require("../constant");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
@@ -13,19 +13,30 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.NODE_ENV ? process.env.GOOGLE_REDIRECT_URL_LOCAL : process.env.GOOGLE_REDIRECT_URL_HOST,
+      callbackURL:
+        process.env.NODE_ENV === "development"
+          ? process.env.GOOGLE_REDIRECT_URL_LOCAL
+          : process.env.GOOGLE_REDIRECT_URL_HOST,
     },
     async function (accessToken, refreshToken, profile, cb) {
       try {
         if (profile?.id) {
           const code = uuidv4();
-          const user = userService.selectUserById(profile.id) 
+          const user = userService.selectUserById(profile.id);
           if (user.length === 0) {
-            await authSocialService.insertUserSocial(profile.id,constant.ROLE_DEFAULT,profile.displayName,profile.emails[0].value,profile.photos[0].value,constant.TYPE_LOG_SOCIAL,code)
-          } else{
-            await authSocialService.updateCode(code,profile.id)
+            await authSocialService.insertUserSocial(
+              profile.id,
+              constant.ROLE_DEFAULT,
+              profile.displayName,
+              profile.emails[0].value,
+              profile.photos[0].value,
+              constant.TYPE_LOG_SOCIAL,
+              code
+            );
+          } else {
+            await authSocialService.updateCode(code, profile.id);
           }
-          profile.code=code;
+          profile.code = code;
           return cb(null, profile);
         }
         return cb(error, profile);
@@ -42,20 +53,31 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: process.env.NODE_ENV ? process.env.FACEBOOK_REDIRECT_URL_LOCAL : process.env.FACEBOOK_REDIRECT_URL_HOST,
-      profileFields: ['id', 'displayName', 'photos', 'emails']
+      callbackURL:
+        process.env.NODE_ENV === "development"
+          ? process.env.FACEBOOK_REDIRECT_URL_LOCAL
+          : process.env.FACEBOOK_REDIRECT_URL_HOST,
+      profileFields: ["id", "displayName", "photos", "emails"],
     },
-   async function (accessToken, refreshToken, profile, cb) {
+    async function (accessToken, refreshToken, profile, cb) {
       try {
         if (profile?.id) {
           const code = uuidv4();
-          const user = await userService.selectUserById(profile.id) 
+          const user = await userService.selectUserById(profile.id);
           if (user.length === 0) {
-            await authSocialService.insertUserSocial(profile.id,constant.ROLE_DEFAULT,profile.displayName,profile.emails[0].value,profile.photos[0].value,constant.TYPE_LOG_SOCIAL,code);
-          }else{
-            await authSocialService.updateCode(code,profile.id);
+            await authSocialService.insertUserSocial(
+              profile.id,
+              constant.ROLE_DEFAULT,
+              profile.displayName,
+              profile.emails[0].value,
+              profile.photos[0].value,
+              constant.TYPE_LOG_SOCIAL,
+              code
+            );
+          } else {
+            await authSocialService.updateCode(code, profile.id);
           }
-          profile.code=code
+          profile.code = code;
           return cb(null, profile);
         }
         return cb(error, profile);
